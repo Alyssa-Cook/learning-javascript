87e720e83053d18b0f21b364659b5b07af0c3272
Alyssa Cook <aly.cook1199@gmail.com>
2023-08-18 21:12:46 -0700

    First try at advent of code problem

--- Projects/advent-of-code/2022-12-01.js
+++ Projects/advent-of-code/2022-12-01.js
@@ -1,1 +1,10 @@
 import { readFileLines } from "../../lib/files.js";
+
+let calories = readFileLines("adventofcode.com_2022_day_1_input.txt")
+
+while (true) {
+    if (isNumeric(calories)){
+        calories = calories + calories
+    }
+}
+console.log(calories)

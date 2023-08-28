7766b61c7577ee9259a21ed9c29b692679001adf
Alyssa Cook <aly.cook1199@gmail.com>
2023-08-27 23:47:54 -0700

    added corect loop

--- Projects/advent-of-code/2022-12-01.js
+++ Projects/advent-of-code/2022-12-01.js
@@ -2,9 +2,17 @@ import { readFileLines } from "../../lib/files.js";
 
 let calories = readFileLines("adventofcode.com_2022_day_1_input.txt")
 
-while (true) {
-    if (isNumeric(calories)){
-        calories = calories + calories
-    }
+
+let input = [""]
+
+let i = 0
+
+let current = 0
+while(i<input.length){
+
+    let num = Number.parseInt (calories [i])
+    current = current + num 
+    i = i + 1
+    continue;
 }
-console.log(calories)
+current 
